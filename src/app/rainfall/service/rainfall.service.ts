d4eb8c362fc51b3dc04cac7d9e1359c369a58b9d
chinmaybansod <chinmay25bansod@gmail.com>
2024-07-02 00:34:48 +0530

    Weather API updated

--- src/app/rainfall/service/rainfall.service.ts
+++ src/app/rainfall/service/rainfall.service.ts
@@ -10,7 +10,7 @@ export class RainfallService {
   constructor(private http: HttpClient) { }
 
   getCurrentWeather(): Observable<any> {
-    const url = 'http://api.weatherapi.com/v1/forecast.json?key=decdaced4749419e8dd162907240107&q=Pune'
+    const url = 'https://api.weatherapi.com/v1/forecast.json?key=decdaced4749419e8dd162907240107&q=Pune'
     return this.http.get(url);
   }
 }
